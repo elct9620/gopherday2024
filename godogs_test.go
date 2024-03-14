bc222425f017b26b85da6df6c4df95c9eabe4193
Aotokitsuruya <contact@aotoki.me>
2024-03-14 22:30:32 +0800

    Refactor godogs to use context.Context

--- godogs_test.go
+++ godogs_test.go
@@ -1,9 +1,12 @@
 package app_test
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
@@ -23,125 +26,174 @@ var opts = godog.Options{
 
 const SuiteSuccessCode = 0
 
-type HttpFeature struct {
-	server   http.Handler
-	response *httptest.ResponseRecorder
-}
+type apiServerCtxKey struct{}
 
-func (feat *HttpFeature) iMakeAGETRequestTo(url string) error {
-	req := httptest.NewRequest("GET", url, nil)
-	feat.response = httptest.NewRecorder()
-
-	feat.server.ServeHTTP(feat.response, req)
-	return nil
-}
-
-func (feat *HttpFeature) iMakeAPOSTRequestToWithTheBody(url string, doc *godog.DocString) error {
-	req := httptest.NewRequest("POST", url, strings.NewReader(doc.Content))
-	req.Header.Add("Content-Type", "application/json")
-	feat.response = httptest.NewRecorder()
-
-	feat.server.ServeHTTP(feat.response, req)
-	return nil
-}
-
-func (feat *HttpFeature) iMakeAPUTRequestToWithTheBody(url string, doc *godog.DocString) error {
-	req := httptest.NewRequest("PUT", url, strings.NewReader(doc.Content))
-	req.Header.Add("Content-Type", "application/json")
-	feat.response = httptest.NewRecorder()
-
-	feat.server.ServeHTTP(feat.response, req)
-	return nil
-}
-
-func (feat *HttpFeature) theResponseStatusCodeShouldBe(statusCode int) error {
-	if feat.response.Code != statusCode {
-		return fmt.Errorf("expected response code to be %d, but actual is %d", statusCode, feat.response.Code)
+func getApiServer(ctx context.Context) (http.Handler, error) {
+	v, ok := ctx.Value(apiServerCtxKey{}).(http.Handler)
+	if !ok {
+		return nil, fmt.Errorf("api server not found in context")
 	}
-	return nil
+
+	return v, nil
 }
 
-func (feat *HttpFeature) theResponseBodyShouldBe(doc *godog.DocString) error {
-	var expected any
-	var actual any
+type apiResponseCtxKey struct{}
 
+func getApiResponse(ctx context.Context) (*httptest.ResponseRecorder, error) {
+	v, ok := ctx.Value(apiResponseCtxKey{}).(*httptest.ResponseRecorder)
+	if !ok {
+		return nil, fmt.Errorf("api response not found in context")
+	}
+
+	return v, nil
+}
+
+func getApiResponseJson(ctx context.Context) (any, error) {
+	res, err := getApiResponse(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var body io.Reader = bytes.NewBufferString(res.Body.String())
+	var actual any
+	if json.NewDecoder(body).Decode(&actual) != nil {
+		return nil, fmt.Errorf("actual response body is not a valid JSON: %s", res.Body.String())
+	}
+
+	return actual, nil
+}
+
+func iMakeAGETRequestTo(ctx context.Context, url string) (context.Context, error) {
+	server, err := getApiServer(ctx)
+	if err != nil {
+		return ctx, err
+	}
+
+	req := httptest.NewRequest("GET", url, nil)
+	res := httptest.NewRecorder()
+	server.ServeHTTP(res, req)
+
+	return context.WithValue(ctx, apiResponseCtxKey{}, res), nil
+}
+
+func iMakeARequestToWithTheBody(ctx context.Context, method, url string, doc *godog.DocString) (context.Context, error) {
+	server, err := getApiServer(ctx)
+	if err != nil {
+		return ctx, err
+	}
+
+	req := httptest.NewRequest(method, url, strings.NewReader(doc.Content))
+	req.Header.Add("Content-Type", "application/json")
+	res := httptest.NewRecorder()
+	server.ServeHTTP(res, req)
+
+	return context.WithValue(ctx, apiResponseCtxKey{}, res), nil
+}
+
+func theResponseStatusCodeShouldBe(ctx context.Context, statusCode int) (context.Context, error) {
+	res, err := getApiResponse(ctx)
+	if err != nil {
+		return ctx, err
+	}
+
+	if res.Code != statusCode {
+		return ctx, fmt.Errorf("expected response code to be %d, but actual is %d", statusCode, res.Code)
+	}
+
+	return ctx, nil
+}
+
+func theResponseBodyShouldBe(ctx context.Context, doc *godog.DocString) error {
+	res, err := getApiResponse(ctx)
+	if err != nil {
+		return err
+	}
+
+	var expected any
 	if json.Unmarshal([]byte(doc.Content), &expected) != nil {
 		return fmt.Errorf("expected response body to be a valid JSON, but actual is %s", doc.Content)
 	}
 
-	actualBody := feat.response.Body.String()
-	if json.Unmarshal([]byte(actualBody), &actual) != nil {
-		return fmt.Errorf("actual response body is not a valid JSON: %s", actualBody)
+	actual, err := getApiResponseJson(ctx)
+	if err != nil {
+		return err
 	}
 
 	if !reflect.DeepEqual(expected, actual) {
-		return fmt.Errorf("expected response body to be %s, but actual is %s", doc.Content, actualBody)
+		return fmt.Errorf("expected response body to be %s, but actual is %s", doc.Content, res.Body.String())
 	}
 
 	return nil
 }
 
-func (feat *HttpFeature) theResponseJsonShouldHave(path string) error {
-	var actual any
-
-	actualBody := feat.response.Body.String()
-	if json.Unmarshal([]byte(actualBody), &actual) != nil {
-		return fmt.Errorf("actual response body is not a valid JSON: %s", feat.response.Body.String())
+func theResponseJsonShouldHave(ctx context.Context, path string) error {
+	res, err := getApiResponse(ctx)
+	if err != nil {
+		return err
 	}
 
-	res, err := jmespath.Search(path, actual)
+	actual, err := getApiResponseJson(ctx)
+	if err != nil {
+		return err
+	}
+
+	found, err := jmespath.Search(path, actual)
 	if err != nil {
 		return fmt.Errorf("failed to search path %s: %s", path, err)
 	}
 
-	if res == nil {
-		return fmt.Errorf(`expected response body to have JMESPath "%s", but actual is %s`, path, actualBody)
+	if found == nil {
+		return fmt.Errorf(`expected response body to have JMESPath "%s", but actual is %s`, path, res.Body.String())
 	}
 
 	return nil
 }
 
-func (feat *HttpFeature) theResponseJsonShouldHaveWithValue(path, value string) error {
-	var actual any
-
-	actualBody := feat.response.Body.String()
-	if json.Unmarshal([]byte(actualBody), &actual) != nil {
-		return fmt.Errorf("actual response body is not a valid JSON: %s", feat.response.Body.String())
+func theResponseJsonShouldHaveWithValue(ctx context.Context, path, value string) error {
+	res, err := getApiResponse(ctx)
+	if err != nil {
+		return err
 	}
 
-	res, err := jmespath.Search(path, actual)
+	actual, err := getApiResponseJson(ctx)
+	if err != nil {
+		return err
+	}
+
+	found, err := jmespath.Search(path, actual)
 	if err != nil {
 		return fmt.Errorf("failed to search path %s: %s", path, err)
 	}
 
-	if res == nil {
-		return fmt.Errorf(`expected response body to have JMESPath "%s", but actual is %s`, path, actualBody)
+	if found == nil {
+		return fmt.Errorf(`expected response body to have JMESPath "%s", but actual is %s`, path, res.Body.String())
 	}
 
-	if fmt.Sprintf("%v", res) != value {
+	if fmt.Sprintf("%v", found) != value {
 		return fmt.Errorf(`expected response body to have JMESPath "%s" with value "%s", but actual is %v`, path, value, res)
 	}
 
 	return nil
 }
 
-func InitializeScenario(ctx *godog.ScenarioContext) {
+func setupHttpServer(ctx context.Context, sc *godog.Scenario) (context.Context, error) {
 	rest, err := app.InitializeTest()
 	if err != nil {
-		panic(err)
+		return ctx, err
 	}
 
-	httpFeat := &HttpFeature{
-		server: rest,
-	}
+	return context.WithValue(ctx, apiServerCtxKey{}, rest), nil
+}
 
-	ctx.Step(`^I make a GET request to "([^"]*)"$`, httpFeat.iMakeAGETRequestTo)
-	ctx.Step(`^I make a POST request to "([^"]*)" with the body$`, httpFeat.iMakeAPOSTRequestToWithTheBody)
-	ctx.Step(`^I make a PUT request to "([^"]*)" with the body$`, httpFeat.iMakeAPUTRequestToWithTheBody)
-	ctx.Step(`^the response status code should be (\d+)$`, httpFeat.theResponseStatusCodeShouldBe)
-	ctx.Step(`^the response body should be$`, httpFeat.theResponseBodyShouldBe)
-	ctx.Step(`^the response json should have "([^"]*)"$`, httpFeat.theResponseJsonShouldHave)
-	ctx.Step(`^the response json should have "([^"]*)" with value "([^"]*)"$`, httpFeat.theResponseJsonShouldHaveWithValue)
+func InitializeScenario(ctx *godog.ScenarioContext) {
+	ctx.Before(setupHttpServer)
+
+	ctx.Step(`^I make a GET request to "([^"]*)"$`, iMakeAGETRequestTo)
+	ctx.Step(`^I make a (POST|PUT) request to "([^"]*)" with the body$`, iMakeARequestToWithTheBody)
+	ctx.Step(`^the response status code should be (\d+)$`, theResponseStatusCodeShouldBe)
+	ctx.Step(`^the response body should be$`, theResponseBodyShouldBe)
+	ctx.Step(`^the response json should have "([^"]*)"$`, theResponseJsonShouldHave)
+	ctx.Step(`^the response json should have "([^"]*)" with value "([^"]*)"$`, theResponseJsonShouldHaveWithValue)
 }
 
 func init() {
